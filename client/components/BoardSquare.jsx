5197df260358ae93649511f91db98154141ab510
Gabriel <gabriel.griffin@epitech.eu>
2022-01-14 11:09:20 +0100

    client/components/ - U: added algebraic notation on certain squares

--- client/components/BoardSquare.jsx
+++ client/components/BoardSquare.jsx
@@ -11,6 +11,9 @@ import Promote from './Promote';
 const BoardSquare = ({piece, isBlack, position}) => {
 
     const [promotion, setPromotion] = useState(null);
+    const [file, setFile] = useState(position[0]);
+    const [rank, setRank] = useState(position[1]);
+    const [squareNotation, setSquareNotation] = useState([]);
 
     const [ , drop] = useDrop({
         accept: 'piece',
@@ -30,6 +33,20 @@ const BoardSquare = ({piece, isBlack, position}) => {
             }
         })
 
+
+        let squareNotationText = "";
+
+        setFile(position[0]);
+        setRank(position[1]);
+        if (position[1] === '1') {
+            squareNotationText = position[0];
+        }
+        if (position[0] === 'a') {
+            squareNotationText += position[1];
+        }
+        setSquareNotation(squareNotationText);
+
+
         // * unsub once you stop rendering
         return (() => subscription.unsubscribe());
     }, [position]);
@@ -37,11 +54,13 @@ const BoardSquare = ({piece, isBlack, position}) => {
     return (
         <Box width='100%' height='100%' ref={drop}>
             <Square isBlack={isBlack}>
-                {/* ! you need to figure this shit out it is driving me insane, have the text show behind the pieces */}
-                {/* <Text color='black'>
-                    {square.file}
-                    {square.rank}
-                </Text> */}
+                {/* //! you need to figure this shit out it is driving me insane, have the text show behind the pieces */}
+                {
+                    (!promotion && squareNotation.length > 0) &&
+                        <Text position={'fixed'} color={isBlack ? "black" : "white"} >
+                            {squareNotation}
+                        </Text>
+                }
 
                 { promotion && <Promote promotion={promotion}/>}
 
