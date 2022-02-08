e83deb9cff531350a5b92e4cb9c2d919012003fb
Gabriel <gabriel.griffin@epitech.eu>
2022-02-08 11:42:16 +0100

    server - U/C: installed necessary api packages

--- server/index.js
+++ server/index.js
@@ -1,4 +1,49 @@
-// * cf dashboard and this:
+const express = require('express')
+const cors = require('cors')
+const swaggerUi = require('swagger-ui-express');
 
-// * https://github.com/ebemunk/node-uci
-// * https://github.com/scottie1984/swagger-ui-express
+// const swaggerDocument = require('./doc/swagger');
+
+const port = 3000;
+
+// const userRouter = require('./routes/user-router');
+// const jwtRouter = require('./routes/jwt-router');
+// const nbaRouter = require('./routes/nba-router');
+// const cryptoRouter = require('./routes/crypto-router');
+// const unsplashRouter = require('./routes/unsplash-router');
+// const hearthstoneRouter = require('./routes/hearthstone-router');
+// const quotesRouter = require('./routes/quotes-router');
+// const prefsRouter = require('./routes/user-prefs-router');
+// const aboutRouter = require('./routes/about-router');
+
+const app = express();
+if (port == undefined) {
+    console.log("DASHBOARD SERVER PORT NOT FOUND");
+    port = 3000;
+}
+
+const apiPort = port
+
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(express.json())
+// app.use("/api/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/api', (req, res) => {
+    res.send('Hello World!')
+})
+
+// app.use('/api', userRouter);
+// app.use('/jwt', jwtRouter);
+// app.use(
+//     userRouter,
+//     jwtRouter,
+//     nbaRouter,
+//     cryptoRouter,
+//     unsplashRouter,
+//     hearthstoneRouter,
+//     quotesRouter,
+//     prefsRouter,
+//     aboutRouter);
+
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
